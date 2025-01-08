4e0cf3910f3cb545d7e0464d4d3bd500fa3d8e49
yeah-caleb <caleb.meeker@mvschool.com>
2025-01-08 18:43:23 -0500

    Auto Functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CompetitionJarlsbergianTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CompetitionJarlsbergianTeleop.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsytems.Drivetrain;
+import org.firstinspires.ftc.teamcode.Subsytems.JarlsArm;
 import org.firstinspires.ftc.teamcode.Subsytems.Odometry;
 
 @TeleOp(name = "YARRRRRG")
@@ -12,21 +13,21 @@ public class CompetitionJarlsbergianTeleop extends OpMode {
 
     Drivetrain drivetrain;
     Odometry odo;
-    //CheetoFingers arm;
+    //JarlsArm arm;
 
     @Override
     public void init() {
         drivetrain = new Drivetrain(hardwareMap);
         odo = new Odometry(hardwareMap);
-        //arm = new CheetoFingers(hardwareMap);
+        //arm = new JarlsArm(hardwareMap);
         odo.resetEncoders();
     }
 
     @Override
     public void loop() {
         drivetrain.GamepadInputs(odo.cur0, gamepad1);
-        //arm.gamepadInputsArm(gamepad1);
         odo.updateCurPos();
+        //arm.gamepadInputs(gamepad1);
 
         telemetry.addData("Rotation", Math.toDegrees(odo.cur0));
         telemetry.addData("X Value", odo.Xc);
@@ -37,6 +38,9 @@ public class CompetitionJarlsbergianTeleop extends OpMode {
         telemetry.addData("odoleft", odo.odoLeft.getCurrentPosition());
         telemetry.addData("odoback", odo.odoBack.getCurrentPosition());
 
+
+        //telemetry.addData("wrist", arm.leftWrist.getPosition());
+        telemetry.addData("oooooh", gamepad1.dpad_up);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsytems/JarlsArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsytems/JarlsArm.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.Subsytems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 public class JarlsArm {
@@ -9,30 +12,80 @@ public class JarlsArm {
     DcMotorEx rightMotor;
     DcMotorEx leftMotor;
 
-    ServoImplEx leftElbow;
-    ServoImplEx rightElbow;
+    Servo leftElbow;
+    Servo rightElbow;
 
-    ServoImplEx leftArm;
-    ServoImplEx rightArm;
+    Servo leftArm;
+    Servo rightArm;
 
-    ServoImplEx leftWrist;
-    ServoImplEx rightWrist;
+    public Servo leftWrist;
+    public Servo rightWrist;
 
-    ServoImplEx claw;
+    CRServo claw;
 
-    JarlsArm(HardwareMap hwMap){
+    double elbowPos;
+    double forearmPos;
+    double wristPos;
+    double clawPow;
+
+    public JarlsArm(HardwareMap hwMap){
         rightMotor = hwMap.get(DcMotorEx.class, "rightArm");
         leftMotor = hwMap.get(DcMotorEx.class, "leftArm");
 
-        rightElbow = hwMap.get(ServoImplEx.class, "RElb");
-        leftElbow = hwMap.get(ServoImplEx.class, "LElb");
-        rightArm = hwMap.get(ServoImplEx.class, "RLink");
-        leftArm = hwMap.get(ServoImplEx.class, "LLink");
-        rightWrist = hwMap.get(ServoImplEx.class, "RWrist");
-        leftWrist = hwMap.get(ServoImplEx.class, "LWrist");
+        rightElbow = hwMap.get(Servo.class, "RElb");
+        leftElbow = hwMap.get(Servo.class, "LElb");
+        rightArm = hwMap.get(Servo.class, "RLink");
+        leftArm = hwMap.get(Servo.class, "LLink");
+        rightWrist = hwMap.get(Servo.class, "RWrist");
+        leftWrist = hwMap.get(Servo.class, "LWrist");
 
-        claw = hwMap.get(ServoImplEx.class, "claw");
+        claw = hwMap.get(CRServo.class, "Claw");
+
+        rightElbow.setDirection(Servo.Direction.REVERSE);
+        rightWrist.setDirection(Servo.Direction.REVERSE);
+
+        rightMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        rightArm.setPosition(0.5);
+        leftArm.setPosition(0.5);
+
     }
 
+
+    public void gamepadInputs(Gamepad gmpad){
+
+        double uppies = gmpad.left_trigger - gmpad.right_trigger;
+
+        leftMotor.setPower(uppies);
+        rightMotor.setPower(uppies);
+
+        if(gmpad.left_bumper){
+            elbowPos += 0.05;
+        }
+        if(gmpad.right_bumper){
+            elbowPos -= 0.05;
+        }
+
+        if(gmpad.dpad_up){
+            wristPos += 0.05;
+        }
+        else if(gmpad.dpad_down){
+            wristPos -= 0.05;
+        }
+
+        //if(gmpad.a){
+
+        //}
+
+        leftElbow.setPosition(elbowPos);
+        rightElbow.setPosition(elbowPos);
+
+        rightWrist.setPosition(0.5);
+        leftWrist.setPosition(0.5);
+
+        //claw.setPower(1);
+
+
+    }
 
 }
