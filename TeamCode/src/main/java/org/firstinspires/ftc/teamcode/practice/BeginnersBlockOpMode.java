adca96fd59e3b20fd67a22d9f9e9cdd32a16ba64
trewalco84 <easycalps2@gmail.com>
2025-02-13 20:55:28 -0500

    Sticking my teleop onto my branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/BeginnersBlockOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/BeginnersBlockOpMode.java
@@ -6,11 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@Teleop (name = BeginnersBlockOpMode)
-public class BeginnersBlockOpMode
+@TeleOp (name = "BeginnersBlockOpMode")
+public class BeginnersBlockOpMode extends OpMode
 {
-    public Gamepad Blocko;
-    Blocko = gamepad1;
+    Gamepad Blocko = gamepad1;
 
     public DcMotorEx rightFront;
     public DcMotorEx leftFront;
@@ -30,35 +29,39 @@ public class BeginnersBlockOpMode
         double lx = Blocko.left_stick_x;
         double ly = Blocko.left_stick_y;
 
-        if(lx > 0) {
+        if(lx > 0)
+        {
             rightFront.setPower(-lx);
             leftFront.setPower(lx);
             rightRear.setPower(lx);
             leftRear.setPower(-lx);
         }
 
-        if(ly > 0) {
+        if(ly > 0)
+        {
             rightFront.setPower(ly);
             leftFront.setPower(ly);
             rightRear.setPower(ly);
             leftRear.setPower(ly);
         }
 
-        if(lx < 0) {
+        if(lx < 0)
+        {
             rightFront.setPower(lx);
             leftFront.setPower(-lx);
             rightRear.setPower(-lx);
             leftRear.setPower(lx);
         }
 
-        if(ly < 0) {
+        if(ly < 0)
+        {
             rightFront.setPower(-ly);
             leftFront.setPower(-ly);
             rightRear.setPower(-ly);
             leftRear.setPower(-ly);
         }
 
-        double rx = Blocko.right_stick_x
+        double rx = Blocko.right_stick_x;
 
         if(rx > 0)
         {
